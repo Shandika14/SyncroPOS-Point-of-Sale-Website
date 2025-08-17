e38bbd81d01c0521cc5ae0d9b4509c2c4bb1ff34
Shandika <sandikasayyidammar96@gmail.com>
2025-08-17 20:08:21 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="c-footer">
+    <div><p class="mt-5 mb-3 text-muted footer-text">&copy; 2024 SyncroPOS. All Rights Reserved.</p>
+    </strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
+
+
+<style>
+  .footer-text {
+    color: white;
+  }
+</style>
+
+<!-- <p class="mt-5 mb-3 text-muted footer-text">&copy; 2024 Presensia. All Rights Reserved.</p> -->
